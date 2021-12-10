3861c98cc44411a1c38b01c98d14a4e5eea94971
Andreas Alexander <72374364+xiao-dre@users.noreply.github.com>
2021-12-10 13:34:56 +0700

    Update main.py

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -9,14 +9,19 @@ from tensorflow.python.keras.preprocessing import image
 from tensorflow.python.keras.models import load_model
 from tensorflow.python.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input as mobilenet_v2_preprocess_input
 
-model = load_model('../Model/model.hdf5')
+st.title('Corona Disease Detector')
+st.write('This is a prototype of the Corona Disease Detector.')
+st.write('Please upload an x-ray to diagnose.')
+st.write('The classification result will be displayed below.')
+
+model = load_model('../Model/myModel.hdf5')
 
 #load file
-uploaded_file = st.file_uploader('Choose an image file', type='jpeg')
+uploaded_file = st.file_uploader('Choose an image file', type=['jpeg', 'jpg', 'png'])
 
 map_dict = {
     0: 'NEGATIVE',
-    1: 'POSITIVE'
+    1: 'POSITIVE',
 }
 
 # print(cv2.__version__)
@@ -29,11 +34,11 @@ if(uploaded_file is not None):
     resized = cv2.resize(opencv_image, (224, 224))
 
     #do something to the image, lets say we display it
-    st.image(opencv_image, channels='RGB')
+    st.image(opencv_image, channels='RGBA')
     resized = mobilenet_v2_preprocess_input(resized)
     img_reshape = resized[np.newaxis, ...]
 
     Generate_pred = st.button('Generate Prediction')
     if(Generate_pred):
         prediction = model.predict(img_reshape).argmax()
-        st.title('Predicted Covid Status is {}'.format(map_dict[prediction]))
+        st.info('Predicted Covid Status is {}'.format(map_dict[prediction]))
